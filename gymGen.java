5cceb3dd66289f5e2be5dceab6f3d9242d76aefe
gaespinoza <gabantesp@gmail.com>
2021-05-16 18:42:00 -0400

    started gen, changes to database

--- gymGen.java
+++ gymGen.java
@@ -3,7 +3,18 @@ import java.io.*;
 import java.util.Random;
 import java.text.DecimalFormat;
 public class gymGen {
-    // private static int maxUser = 100, maxGymnast = 1000, maxTeam = 20000, maxLeague = 
+    private static int maxUser = 1000, maxGymnast = 1000, maxTeam = 1000, maxLeague = 1000, 
+        maxEmail = 1000, maxEvents = 6, maxName = 1000, maxTeam = 1000, maxLocation = 1000;
+    private static int maxYear = 2030, minYear = 2020;
+    private static int maxID = 99999999;
+    private static Random rnd = new Random();
+    private static String[] userNameArray = new String[maxUser];
+    private static String[] nameArray = new String[maxName];
+    private static String[] emailArray = new String[maxEmail];
+    private static String[] leagueArray = new String[maxLeague];
+    private static String[] teamArray = new String[maxTeam];
+    private static String[] locationArray = new String[maxLocation];
+    private static String[] eventArray = new String[maxEvents];
 
     private static Scanner openFile(String fileName) {
         Scanner in = null;
@@ -29,45 +40,61 @@ public class gymGen {
     }
     private static void fillArrays() {
     int i;
+    // fill usernames
+    Scanner in = openFile("userNames");
+    i = 0;
+    while (in.hasNext() && i < maxUser) {
+      userNameArray[i++] = in.nextLine();
+    }
+    if (i < maxUser) maxUser = i;
+    in.close();
     // fill first names
-    Scanner in = openFile("buildingNames");
-    i = 0;
-    while (in.hasNext() && i < maxBuilding) {
-      buildingArray[i++] = in.nextLine();
-    }
-    if (i < maxBuilding) maxBuilding = i;
-    in.close();
-    // fill last names
-    Scanner in = openFile("buildingNames");
-    i = 0;
-    while (in.hasNext() && i < maxBuilding) {
-      buildingArray[i++] = in.nextLine();
-    }
-    if (i < maxBuilding) maxBuilding = i;
-    in.close();
-    // fill emails
-    Scanner in = openFile("buildingNames");
-    i = 0;
-    while (in.hasNext() && i < maxBuilding) {
-      buildingArray[i++] = in.nextLine();
-    }
-    if (i < maxBuilding) maxBuilding = i;
-    in.close();
-    // fill team/league names
-    in = openFile("personNames");
+    Scanner in = openFile("firstNames");
     i = 0;
     while (in.hasNext() && i < maxName) {
       nameArray[i++] = in.nextLine();
     }
     if (i < maxName) maxName = i;
     in.close();
-    // fill events names
-    Scanner in = openFile("buildingNames");
+    // fill emails
+    Scanner in = openFile("emails");
     i = 0;
-    while (in.hasNext() && i < maxBuilding) {
-      buildingArray[i++] = in.nextLine();
+    while (in.hasNext() && i < maxEmail) {
+      emailArray[i++] = in.nextLine();
     }
-    if (i < maxBuilding) maxBuilding = i;
+    if (i < maxEmail) maxEmail = i;
+    in.close();
+    // fill league names
+    in = openFile("leagueName");
+    i = 0;
+    while (in.hasNext() && i < maxLeague) {
+      leagueArray[i++] = in.nextLine();
+    }
+    if (i < maxLeague) maxLeague = i;
+    in.close();
+    // fill team names
+    in = openFile("teamNames");
+    i = 0;
+    while (in.hasNext() && i < maxTeam) {
+      teamArray[i++] = in.nextLine();
+    }
+    if (i < maxTeam) maxTeam = i;
+    in.close();
+    // fill location names
+    in = openFile("locationName");
+    i = 0;
+    while (in.hasNext() && i < maxLocation) {
+      locationArray[i++] = in.nextLine();
+    }
+    if (i < maxLocation) maxLocation = i;
+    in.close();
+    // fill events names
+    Scanner in = openFile("eventNames");
+    i = 0;
+    while (in.hasNext() && i < maxEvents) {
+      eventArray[i++] = in.nextLine();
+    }
+    if (i < maxEvents) maxEvents = i;
     in.close();
   }
 
@@ -77,7 +104,19 @@ public class gymGen {
         int gym_num = 1;
         fillArrays();
         PrintWriter out = outputFile("largeRelationsInsertFile.sql");
-        for 
+        out.println("delete from users;");
+        out.println("delete from league;");
+        out.println("delete from team;")
+        out.println("delete from gymnast;")
+        out.println("delete from lineup_slot;")
+        out.println("delete from score;")
+        out.println("delete from user_league;")
+        out.println("delete from roster;")
+        out.println("delete from matchup;")
+
+        for (i=0; i < 10; i++) {
+            
+        } 
     }
 
 }
