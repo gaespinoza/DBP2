fb30a2da460c03c29a2f7ec17219511462db8617
Daniel Quiroga <dquiroga@email.wm.edu>
2021-05-17 00:34:47 -0400

    info

--- gymGen.java
+++ gymGen.java
@@ -268,7 +268,7 @@ public class gymGen {
 
         createGymnasts(out);
 
-        for (int i=0; i < 2; i++) {
+        for (int i=0; i < 1; i++) {
 
             String[] teams = createLeague(out, i+1);
             for (int j=0; j < 5; j++){
